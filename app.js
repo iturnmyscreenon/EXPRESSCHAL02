d4be73d0de9bee4b1aad6cf47045a9f5bbd95f3e
iturnmyscreenon <97238230+iturnmyscreenon@users.noreply.github.com>
2022-11-07 12:15:08 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.APP_PORT || 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+const userHandlers = require("./userHandlers");
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+
+app.listen(port, (err) => { 
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
